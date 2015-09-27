edecd5bd0fb467282ceec219f811851edad301d9
Carlos Macasaet <github@carlos-m.net>
2015-09-26 22:36:33 -0700

    Jersey 2 Compatibility #47 - remove unnecessary logging

--- server2/src/main/java/com/bazaarvoice/auth/hmac/server/PrincipalInjectionResolver.java
+++ server2/src/main/java/com/bazaarvoice/auth/hmac/server/PrincipalInjectionResolver.java
@@ -3,8 +3,6 @@ package com.bazaarvoice.auth.hmac.server;
 import javax.inject.Singleton;
 
 import org.glassfish.jersey.server.internal.inject.ParamInjectionResolver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * {@link org.glassfish.hk2.api.InjectionResolver InjectionResolver} for
@@ -13,11 +11,8 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class PrincipalInjectionResolver extends ParamInjectionResolver<HmacAuth> {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
     public PrincipalInjectionResolver() {
         super(PrincipalValueFactoryProvider.class);
-        logger.debug("PrincipalInjectionResolver is ready");
     }
 
     public boolean isMethodParameterIndicator() {
